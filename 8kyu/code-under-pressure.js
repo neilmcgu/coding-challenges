29b45fe075ab54d2f76deb991efe581bab6e8db5
NM Desktop <neil@inara.world>
2022-11-16 17:44:14 +0700

    codewars 8kyu 11-16-22

---
+++ 8kyu/code-under-pressure.js
@@ -0,0 +1,14 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    i *= 2
+    return i;
+  }
+
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    i *= 2
+    return i*2;
+  }
