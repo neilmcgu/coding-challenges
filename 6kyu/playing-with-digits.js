ac7a0381cb2185b65a61f74cb2c72ac44d6e0a6f
Neil McGuire <neil@neilmcguire.info>
2022-12-20 11:15:22 +0700

    playing-with-digits

---
+++ 6kyu/playing-with-digits.js
@@ -0,0 +1,45 @@
+//playing-with-digits
+
+// Some numbers have funny properties. For example:
+
+// 89 --> 8¹ + 9² = 89 * 1
+
+// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+// we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+// In other words:
+
+// Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be given as strictly positive integers.
+
+// digPow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
+// digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+// digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+// digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+function digPow(n, p){
+  // ...
+  let k = 0
+  let string = String(n)
+  console.log(string)
+  let arr = []
+  for(let i = 0; i<string.length; i++){
+    arr.push(Number(string[i]))
+  }
+  console.log(arr)
+  for( let i = 0; i<arr.length; i++){
+    k+= Math.pow(arr[i], p+i)
+  }
+  if( k % n == 0){
+    return k / n
+  }else{
+    return -1
+  }
+}
