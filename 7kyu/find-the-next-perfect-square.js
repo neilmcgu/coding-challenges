5f041c60def91dc1ffc4b2f3ab1c51955fe22793
Neil McGuire <neil@neilmcguire.info>
2022-12-23 11:10:52 +0700

    reverse words and sum of nth series

--- 7kyu/find-the-next-perfect-square.js
+++ 7kyu/find-the-next-perfect-square.js
@@ -1,6 +1,6 @@
-// find-the-next-perfect-square
+// find-the-next-perfect-square//////////////
 
-// You might know some pretty large perfect squares. But what about the NEXT one?
+/// You might know some pretty large perfect squares. But what about the NEXT one?
 
 // Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
 
