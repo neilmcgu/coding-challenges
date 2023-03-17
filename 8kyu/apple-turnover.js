fd4580993dff7f8b653dc47481da727370cd315e
Neil McGuire <12nmcguire@gmail.com>
2023-03-17 09:13:02 +0700

    apple turnover

---
+++ 8kyu/apple-turnover.js
@@ -0,0 +1,9 @@
+// Apple turnover
+
+function apple(x) {
+  if (x * x > 1000) {
+    return "It's hotter than the sun!!"
+  } else {
+    return "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
+}
