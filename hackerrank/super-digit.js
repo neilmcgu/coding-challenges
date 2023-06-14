6afe111d0b775a6e6fbceb2b510364f93c2e5dbc
Neil McGuire <12nmcguire@gmail.com>
2023-06-14 20:54:06 +0700

    super digit obectified

--- hackerrank/super-digit.js
+++ hackerrank/super-digit.js
@@ -14,3 +14,41 @@ function superDigit(n, k) {
 }
 
 console.log(superDigit(9875, 4));
+
+// objectified
+// super digit
+// given a number n, and another number k times to repeat n to make a bigger nummber
+// add all of the digits of the bigger number together to get a sum
+// if that sum is a single digit, return that sum
+// otherwise add all of the digits of the sum together
+// check to see if the new sum is a single digit
+// rinse and repeat
+
+const superDigit = {
+  n: 0,
+  k: 0,
+  generateNum() {
+    this.num = Number(String(this.n).repeat(this.k));
+  },
+  findSum() {
+    let arr = String(this.num)
+      .split("")
+      .map((num) => Number(num));
+    let sum = arr.reduce((acc, ce) => acc + ce, 0);
+    return sum;
+  },
+  getSuperDigit() {
+    let sum = this.findSum();
+    if (String(sum).length > 1) {
+      this.num = sum;
+      return this.getSuperDigit();
+    } else {
+      return sum;
+    }
+  },
+};
+
+superDigit.n = 123;
+superDigit.k = 2;
+superDigit.generateNum();
+console.log(superDigit.getSuperDigit());
