dde1d5e7f86d165805922f7510c7ea30b7643d48
Neil McGuire <12nmcguire@gmail.com>
2023-07-06 11:35:58 +0700

    comment grid challenge

--- hackerrank/grid-challenge.js
+++ hackerrank/grid-challenge.js
@@ -1,30 +1,30 @@
-function gridChallenge(grid) {
-  // Write your code here
-  // given a grid of rows and columns, alphabetize the rows and then check to see if the columns are also alphabetized.
-  // if both rows and columns are in alphabetical order, return yes. otherwise return no.
-  console.log(grid);
+// function gridChallenge(grid) {
+//   // Write your code here
+//   // given a grid of rows and columns, alphabetize the rows and then check to see if the columns are also alphabetized.
+//   // if both rows and columns are in alphabetical order, return yes. otherwise return no.
+//   console.log(grid);
 
-  let newGrid = grid.map((row) => row.split("").sort().join(""));
-  let columns = [];
-  for (let i = 0; i < newGrid[0].length; i++) {
-    let col = "";
-    newGrid.forEach((row) => {
-      col += row[i];
-    });
-    columns.push(col);
-  }
+//   let newGrid = grid.map((row) => row.split("").sort().join(""));
+//   let columns = [];
+//   for (let i = 0; i < newGrid[0].length; i++) {
+//     let col = "";
+//     newGrid.forEach((row) => {
+//       col += row[i];
+//     });
+//     columns.push(col);
+//   }
 
-  let alphabetical = true;
+//   let alphabetical = true;
 
-  columns.forEach((col) => {
-    if (col !== col.split("").sort().join("")) alphabetical = false;
-  });
+//   columns.forEach((col) => {
+//     if (col !== col.split("").sort().join("")) alphabetical = false;
+//   });
 
-  console.log("newgrid", newGrid);
-  console.log("columns", columns);
+//   console.log("newgrid", newGrid);
+//   console.log("columns", columns);
 
-  return alphabetical ? "YES" : "NO";
-}
+//   return alphabetical ? "YES" : "NO";
+// }
 
 // Objectified
 // grid challenge
@@ -59,7 +59,7 @@ const grid = {
   },
 };
 
-grid.input = ["dfe", "abc", "hji"];
+grid.input = ["abc", "def", "hji"];
 grid.alphabetizeRows();
 grid.getCols();
 console.log(grid.checkCols());
