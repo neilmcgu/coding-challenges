ff710efd0fee85e16fac8b8ba544da6df4a3c61e
Neil McGuire <12nmcguire@gmail.com>
2023-07-06 16:20:49 +0700

    two-sum

---
+++ leetcode/two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for(let i = 0; i<nums.length; i++){
+        for(let j = 0; j<nums.length; j++){
+            if(i != j){
+                let sum = nums[i] + nums[j]
+                if(sum === target){
+                    return [i,j]
+                }
+            }
+        }
+    }
+};
