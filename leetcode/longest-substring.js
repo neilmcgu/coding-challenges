c566c5d4abc9070bd30e9a293d7ebc5e583cdc98
Neil McGuire <12nmcguire@gmail.com>
2023-07-28 13:16:30 +0700

    anki and longest substring

--- leetcode/longest-substring.js
+++ leetcode/longest-substring.js
@@ -2,22 +2,67 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-    let lengthOfLongestSubstring = 0
-    let left = 0
-    let right = 1
-    while(right <= s.length){
-        let substr = s.substring(left,right)
-        if(substr.length > lengthOfLongestSubstring){
-            lengthOfLongestSubstring = substr.length
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set();
+    let left = 0;
+    let maxSize = 0;
+  
+    if (s.length === 0) return 0;
+    if (s.length === 1) return 1;
+  
+    for (let i = 0; i < s.length; i++) {
+  
+        while (set.has(s[i])) {
+            set.delete(s[left])
+            left++;
         }
-        if(s[left] === s[right]){
-            left = right
-        }
-        right ++
+        set.add(s[i]);
+        maxSize = Math.max(maxSize, i - left + 1)
     }
-    return lengthOfLongestSubstring
-}
-
-let s = 'aabcdeabcdefabaa'
-console.log(findLongestSubstringWithoutRepeatingChars(s))
+    return maxSize;
+  }
+  
+  /**
+   * my solution *
+   * @param {string} s
+   * @return {number}
+   */
+  var lengthOfLongestSubstring = function(s) {
+      // given a string s
+      // find the longest substring without repeating characters
+      // return that substring
+      let longest = ''
+  
+      // if all characters are unique, return the original string
+      let unique = true
+      s.split('').forEach(char => {
+          if(s.split('').indexOf(char) !== s.split('').lastIndexOf(char)){
+              unique = false
+          }
+      })
+  
+      if(unique) return s
+      
+      for(let i = 0; i<s.length; i++){
+          for(let j = 0;j<s.length;j++){
+              let substring = s.substring(i,j)
+              if(substring.length > longest.length && charsAreUnique(substring)){
+                  console.log('longest',longest)
+                  console.log('substring',substring)
+                  longest = substring
+              }
+          }
+      }
+  
+      return longest.length
+  };
+  
+  function charsAreUnique(s){
+      let unique = true
+      s.split('').forEach(char => {
+          if(s.split('').indexOf(char) !== s.split('').lastIndexOf(char)){
+              unique = false
+          }
+      })
+      return unique
+  }
