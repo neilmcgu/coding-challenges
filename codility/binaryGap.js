d20eefe162f3ee3c734357424b937965ef9b3c39
Neil McGuire <12nmcguire@gmail.com>
2023-09-24 21:38:35 -0400

    binary gap

---
+++ codility/binaryGap.js
@@ -0,0 +1,12 @@
+/*
+
+binary gap
+take a number (positive int)
+turn it into binary
+return the length of the longest binary gap (binary gap is the number of zeros between ones in a binary number. ex: 1001 => binary gap of two)
+
+*/
+
+function solution(n) {
+  // how to get binary?
+}
