00fbd6ba4db27e6d6e9a2ecf8d70aaaa1952e9fd
Neil McGuire <12nmcguire@gmail.com>
2024-01-06 06:18:18 -0500

    get age

---
+++ codewars/8kyu/getAge.js
@@ -0,0 +1,11 @@
+function getAge(inputString) {
+  // return the girl's correct age as an integer. Happy coding :)
+  let age = inputString.split(" ")[0];
+  return Number(age);
+}
+
+// can also use parseint
+
+const age = (inputString) => {
+  parseInt(inputString);
+};
