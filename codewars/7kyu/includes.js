001b480900da01aed140c8c57282fdf5d9b08a8c
Neil McGuire <12nmcguire@gmail.com>
2024-02-04 07:50:59 +0700

    includes

---
+++ codewars/7kyu/includes.js
@@ -0,0 +1,36 @@
+function reverseLetter(str) {
+  //coding and coding..
+  let alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  return str
+    .split("")
+    .filter((e) => alphabet.includes(e))
+    .reverse()
+    .join("");
+}
