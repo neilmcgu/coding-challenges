dc2e747488aa83cebc051195b67460e61329ed6d
Neil McGuire <12nmcguire@gmail.com>
2024-02-12 09:54:48 +0700

    alternate case

---
+++ codewars/7kyu/alternateCase.js
@@ -0,0 +1,9 @@
+function alternateCase(s) {
+  return s
+    .split("")
+    .map((char) =>
+      char.toLowerCase() === char ? char.toUpperCase() : char.toLowerCase()
+    )
+    .join("");
+  //   return '';
+}
