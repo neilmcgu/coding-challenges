b973aae572dc43111553093b429ba23a20fca726
Neil McGuire <12nmcguire@gmail.com>
2024-03-16 10:30:22 +0700

    three rooms

---
+++ codewars/8kyu/3rooms.js
@@ -0,0 +1,18 @@
+// Add rooms here
+var rooms = {
+  room1: {
+    name: "name",
+    description: "description",
+    completed: false,
+  },
+  room2: {
+    name: "name",
+    description: "description",
+    completed: false,
+  },
+  room3: {
+    name: "name",
+    description: "description",
+    completed: false,
+  },
+};
